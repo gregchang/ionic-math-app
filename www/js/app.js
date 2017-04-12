7c2737633b16bed359f6bba822348cd354354895
Gregory Chang <gcchang2@illinois.edu>
2017-04-11 22:16:47 -0500

    Config update and comment out todo code

--- www/js/app.js
+++ www/js/app.js
@@ -32,29 +32,29 @@ angular.module('todo', ['ionic'])
      */
     .factory('Projects', function() {
         return {
-            all: function() {
-                var projectString = window.localStorage['projects'];
-                if (projectString) {
-                    return angular.fromJson(projectString);
-                }
-                return [];
-            },
-            save: function(projects) {
-                window.localStorage['projects'] = angular.toJson(projects);
-            },
-            newProject: function(projectTitle) {
-                // Add a new project
-                return {
-                    title: projectTitle,
-                    tasks: []
-                };
-            },
-            getLastActiveIndex: function() {
-                return parseInt(window.localStorage['lastActiveProject']) || 0;
-            },
-            setLastActiveIndex: function(index) {
-                window.localStorage['lastActiveProject'] = index;
-            }
+            // all: function() {
+            //     var projectString = window.localStorage['projects'];
+            //     if (projectString) {
+            //         return angular.fromJson(projectString);
+            //     }
+            //     return [];
+            // },
+            // save: function(projects) {
+            //     window.localStorage['projects'] = angular.toJson(projects);
+            // },
+            // newProject: function(projectTitle) {
+            //     // Add a new project
+            //     return {
+            //         title: projectTitle,
+            //         tasks: []
+            //     };
+            // },
+            // getLastActiveIndex: function() {
+            //     return parseInt(window.localStorage['lastActiveProject']) || 0;
+            // },
+            // setLastActiveIndex: function(index) {
+            //     window.localStorage['lastActiveProject'] = index;
+            // }
         }
     })
 
