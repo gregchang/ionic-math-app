8bb2ed547b96ca309a118b74d4b93875e3c36297
Greg Chang <gcchang2@illinois.edu>
2017-09-15 01:32:41 -0500

    Add check to only run splash screen specific code on device

--- www/js/app.js
+++ www/js/app.js
@@ -7,9 +7,11 @@ angular.module('todo', ['ionic', 'firebase', 'ionic-toast'])
     .constant('FirebaseUrl', 'https://mathfast-3bcce.firebaseio.com/')
     .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
-            setTimeout(function() {
-                navigator.splashscreen.hide();
-            }, 300);
+            if (window.cordova) {
+                setTimeout(function() {
+                    navigator.splashscreen.hide();
+                }, 300);
+            }
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
                 // for form inputs)
